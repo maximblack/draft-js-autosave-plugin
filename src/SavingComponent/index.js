0939e4d46c0cd9209043127820e8144b87643180
maximblack <maximblack@gmail.com>
2017-08-18 10:28:35 +0300

    React 16 compatible

--- src/SavingComponent/index.js
+++ src/SavingComponent/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const SavingComponent = ({ saving, getIsClean, theme }) => {
   const saved = getIsClean() && !saving;
